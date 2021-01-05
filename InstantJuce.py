495d17023f5d3095498e7a7417be84929e8ec2c3
ragnaringi <ragnar.hrafnkelsson@gmail.com>
2021-01-05 11:37:14 +0000

    Use local JUCE installation to generate Runtime project

--- InstantJuce.py
+++ InstantJuce.py
@@ -47,12 +47,52 @@ def stop():
 
 if __name__ == "__main__":
 
+    jucePathFile = ".jucepath"
+
+    # Get path to user's JUCE installation
+    if not os.path.exists(jucePathFile):
+        with open(jucePathFile, 'w') as fp: 
+            path = input("Enter path to Projucer.app: ")
+            path = path.strip()
+            print(path)
+            if not os.path.isdir(path):
+                print("Error: Path does not exist. Exiting")
+                os.stat(path)
+                os.remove(jucePathFile)
+                exit()
+
+            fp.write(path)
+    
+    # Read stored Projucer.app path 
+    projucerPath = ""
+    with open(jucePathFile, 'r') as file:
+        projucerPath = file.read().replace('\n', '')
+
+    print("Projucer.app installed at: ", projucerPath)
+    
+    # Check if Projucer is found
+    projucerBinaryPath = os.path.join(projucerPath, "Contents", "MacOS", "Projucer")
+    
+    print("Looking for Projucer binary at: ", projucerBinaryPath)
+
+    if not os.path.exists(projucerBinaryPath):
+        print("Error: Projucer binary not found. Exiting")
+        os.remove(jucePathFile)
+        exit()
+
+    # Generate Xcode Project
+    print("Generating Xcode project")
+    cmd = projucerBinaryPath + " --resave bin/InstantJuce/Runtime.jucer"
+    print(cmd)
+    subprocess.call(cmd, shell=True)
+
     build()
     start()
 
     logging.basicConfig(level=logging.INFO,
                         format='%(asctime)s - %(message)s',
                         datefmt='%Y-%m-%d %H:%M:%S')
+    
     path = sys.argv[1] if len(sys.argv) > 1 else '.'
     event_handler = EventHandler()
     observer = Observer()
